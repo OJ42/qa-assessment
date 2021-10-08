0e4ee63ef168a74cc58f7e59faf63ed7f4c1cec6
OJ42 <mcdavid42@gmail.com>
2021-10-08 10:29:35 -0600

    uploading everything so far

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../tictacjs.html'));
 });
 
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style.css'))
+})
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -18,3 +18,24 @@ test('I can start a game', async () => {
     await button.click();
     
 });
+//First added test
+test('I have a game board', async () => {
+
+    let button = await (await driver).findElement(By.id('game-board'));
+    await button.click();
+    
+});
+//Second added test
+test('Top left cell click', async () => {
+
+    let button = await (await driver).findElement(By.id('cell-0'));
+    await button.click();
+    
+});
+//third added test
+test('Middle right cell click', async () => {
+
+    let button = await (await driver).findElement(By.id('cell-5'));
+    await button.click();
+    
+});
